5f4e07ff597fa4769de222c1cb9085bbded30887
ran avital <ranavitaln@gmail.com>
2022-05-17 21:49:21 +0300

    start

---
+++ src/App.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <div class="container">
+    <h1>Peek-a-vue</h1>
+    <section class="game-board">
+      <Card 
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`" 
+      :value="card.value"
+      :visible="card.visible"
+       :position="card.position"
+       @selectCardClicked="filpCard"
+       />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import Card from "./components/Card.vue";
+import { ref } from 'vue'
+
+
+const cardList = ref([]);
+for (let i = 0; i < 16; i++) {
+  cardList.value.push({
+    value:i,
+    visible:false,
+    position:i
+  });
+
+
+}
+
+
+const filpCard = (position) =>{
+  cardList.value[position].visible=true
+  console.log(position)
+};
+
+
+
+
+
+
+
+</script>
+
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: repeat(4, 100px);
+  grid-column-gap: 30px;
+  grid-row-gap: 30px;
+  justify-content: center;
+}
+
+.container {
+  text-align: center;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="card"
+     @click="handleSelectedCard"
+     >
+        <div 
+        v-if="visible" 
+        class="card-face is-front">
+             {{ value }}
+        </div>
+        <div v-else class="card-face is-back">
+            back
+        </div>
+       
+
+    </div>
+</template>
+
+<script setup>
+
+
+const emit= defineEmits(['selectCardClicked'])
+
+
+const handleSelectedCard = () =>{
+    emit('selectCardClicked',props.position)
+
+}
+
+
+
+const props=defineProps({
+    value:{
+        type:Number,
+        required:true
+    },
+    visible:{
+        type:Boolean,
+        default:false
+    },
+    position:{
+        type:Number,
+        required:true
+    }
+
+});
+
+
+
+</script>
+
+<style>
+.card {
+  border: 5px solid #ccc;
+  position: relative;
+}
+
+.card-face{
+    width:100%;
+    height:100%;
+    position: absolute;
+}
+.card-face.is-front{
+    background-color: red;
+    color: #fff;
+
+}
+
+.card-face.is-back{
+    background-color: blue;
+    color:#fff;
+}
+</style>
+
+
